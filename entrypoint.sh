02f702f82a1382ced0da295cfa0b0b16ea4cb83e
Avior <florian.bouillon@delta-wings.net>
2025-03-07 09:49:11 +0100

    feat: Allow to add custom arguments (#1)
    * Update action.yaml
    
    * Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,8 @@ if [[ ! -f "${INPUT_PROJECTBASEDIR%/}/sonar-project.properties" ]]; then
     -Dsonar.login="${INPUT_LOGIN}" \
     -Dsonar.token="${SONAR_PASSWORD}" \
     -Dsonar.sources="${INPUT_PROJECTBASEDIR}" \
-    -Dsonar.sourceEncoding="${INPUT_ENCODING}"
+    -Dsonar.sourceEncoding="${INPUT_ENCODING}" \
+    ${INPUT_ARGS}
 else
   sonar-scanner \
     -Dsonar.host.url="${INPUT_HOST}" \
